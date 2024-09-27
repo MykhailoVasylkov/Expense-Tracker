70c950e6c60b9636f8d891ad29e3f56b548791a5
Mykhailo Vasylkov <vasilkoff.mihail@gmail.com>
2024-09-27 13:09:52 +0000

    Ceate add_expense function that get data from user and append it to google sheet

--- run.py
+++ run.py
@@ -14,5 +14,25 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Expense Tracker').sheet1  # Open the first sheet
 
-data = SHEET.get_all_values()
-print(data)
+# Function to add a new expense
+# I used Chat-GPT to figure out how return back to the menu
+# I used https://docs.python.org/3/tutorial/errors.html to handle try except block
+
+def add_expense():
+    while True:
+        try:
+            amount = input("\nEnter the expense amount or press 'm' to return to the menu: ")
+            if amount.lower() == 'm':
+                return
+            amount = float(amount) #Conver to floating point number
+            if amount <= 0:
+                print("Error: Amount must be a positive number.")
+                continue
+            break
+        except ValueError:
+            print("Error: Please enter a numeric value for the amount.")
+
+    SHEET.append_row([amount])  # Add data to Google Sheets
+    print("\nExpense added successfully!")
+
+add_expense()
