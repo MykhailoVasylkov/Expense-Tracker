0dd1bdb5c9d33afb47dbca420e70f1d89c7e0ae2
Mykhailo Vasylkov <vasilkoff.mihail@gmail.com>
2024-09-30 14:57:33 +0000

    Make changes to the syntax and code to meet the requirements of PEP 8

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 import gspread
 from google.oauth2.service_account import Credentials
 # Imported datetime module to working with dates and times.
-import datetime   
+import datetime
 
 SCOPE = [
     "https://www.googleapis.com/auth/spreadsheets",
@@ -16,25 +16,30 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Expense Tracker').sheet1  # Open the first sheet
 
 """
-Function to add a new expense
-I used Chat-GPT to figure out how return back to the menu
-I used https://docs.python.org/3/tutorial/errors.html to handle try except block
+Function to add a new expense.
+I used Chat-GPT to figure out how return back to the menu.
+I used https://docs.python.org/3/tutorial/errors.html to handle try except
+block.
 """
+
 
 def add_expense():
     while True:
         try:
-            amount = input("\nEnter the expense amount or press 'm' to return to the menu: ")
+            amount = input(
+                f"Enter the expense amount"
+                f"or press 'm' to return to the menu:"
+            )
             if amount.lower() == 'm':
                 return
-            amount = float(amount) #Convert to floating point number
+            amount = float(amount)  # Convert to floating point number
             if amount <= 0:
                 print("Error: Amount must be a positive number.")
                 continue
             break
         except ValueError:
             print("Error: Please enter a numeric value for the amount.")
-    
+
     categories_dict = {
         "1": "Food",
         "2": "Transport",
@@ -43,14 +48,16 @@ def add_expense():
         "5": "Entertainment/Travel",
         "6": "Health",
         "7": "Other"
-    }
+        }
 
     while True:
-        print("\nEnter the expense category or press 'm' to return to the menu:")
+        print(
+            "\nEnter the expense category or press 'm' to return to the menu:"
+            )
         for key, value in categories_dict.items():
             print(f"{key}. {value}")
         category_input = input("Choose a category (1-7): ")
-        
+
         if category_input.lower() == 'm':
             return
         if category_input in categories_dict:
@@ -58,16 +65,22 @@ def add_expense():
             break
         else:
             print("Error: Invalid category input. Enter a number from 1 to 7.")
-        
-        # I used Chat-GPT to figure out how collect date data using datetime module
-        
+
+        """
+        I used Chat-GPT to figure out how collect date data using
+        datetime module
+        """
+
     while True:
-        date_input = input("\nEnter the date (dd-mm-yyyy) or press Enter for today's date or 'm' to return to the menu: ")
+        date_input = input(
+            f"Enter the date (dd-mm-yyyy) or press Enter for today's"
+            f"date or 'm' to return to the menu: ")
         if date_input.lower() == 'm':
             return
         if date_input:
             try:
-                date = datetime.datetime.strptime(date_input, "%d-%m-%Y").date()
+                date = datetime.datetime.strptime(date_input, "%d-%m-%Y")
+                date = date.date()
                 break
             except ValueError:
                 print("Error: Incorrect date format. Use dd-mm-yyyy.")
@@ -75,23 +88,26 @@ def add_expense():
             date = datetime.date.today().strftime("%d-%m-%Y")
             break
 
-    SHEET.append_row([amount, category, str(date)])  # Add data to Google Sheets
+    # Add data to Google Sheets
+    SHEET.append_row([amount, category, str(date)])
     print("\nExpense added successfully!")
 
+
 # Empty list for expenses
-expenses = [] 
+expenses = []
+
 
 # Function to load and view expenses with filtering options
 def load_and_view_expenses():
     global expenses, SHEET
-  
+
     try:
         # Get all records from Google Sheets
         rows = SHEET.get_all_records()
         if not rows:
             print("Google Sheets is empty or missing headers.")
             return
-    
+
 # Load all expenses from Google Sheets into the local list
         for row in rows:
             expense = {
@@ -102,40 +118,59 @@ def load_and_view_expenses():
             expenses.append(expense)
 
         # Use loop for the menu
-        while True:  
+        while True:
             print("\nFilter expenses:")
             print("1. By date")
             print("2. By category")
             print("3. Show all records")
             print("m. Return to the previous menu")
-            filter_choice = input("Choose a filtering option (1-3 or 'm' to return to the menu): ")
-            
+            filter_choice = input(
+                f"Choose a filtering option"
+                f"(1-3 or 'm' to return to the menu):"
+                )
+
             # I used Chat-GPT to implement filters
             if filter_choice == "1":
                 # Input start date
-                start_date_input = input("Enter start date (dd-mm-yyyy) or 'm' to return to the menu: ")
+                start_date_input = input(
+                    f"Enter start date (dd-mm-yyyy)"
+                    f"or 'm' to return to the menu: "
+                    )
                 if start_date_input.lower() == 'm':
-                    break  
+                    break
                 try:
-                    start_date = datetime.datetime.strptime(start_date_input, "%d-%m-%Y").date()
+                    s_date = datetime.datetime.strptime(
+                        start_date_input, "%d-%m-%Y"
+                        )
+                    s_date = s_date.date()
                 except ValueError:
-                    print("Error: Incorrect date format. Make sure to use dd-mm-yyyy.")
-                    continue  
+                    print(
+                        f"Error: Incorrect date format."
+                        f" Make sure to use dd-mm-yyyy."
+                        )
+                    continue
 
                 # Input end date
-                end_date_input = input("Enter end date (dd-mm-yyyy) or 'm' to return to the menu: ")
+                end_date_input = input(
+                    f"Enter end date (dd-mm-yyyy)"
+                    f" or 'm' to return to the menu: ")
                 if end_date_input.lower() == 'm':
-                    break  
+                    break
                 try:
-                    end_date = datetime.datetime.strptime(end_date_input, "%d-%m-%Y").date()
+                    end_date = datetime.datetime.strptime(
+                               end_date_input, "%d-%m-%Y"
+                               ).date()
                 except ValueError:
-                    print("Error: Incorrect date format. Make sure to use dd-mm-yyyy.")
-                    continue  
+                    print(
+                        f"Error: Incorrect date format."
+                        f" Make sure to use dd-mm-yyyy."
+                        )
+                    continue
 
                 # Filter expenses by entered dates
-                filtered_expenses = filter_expenses_by_date(start_date, end_date)
-            
-            #Filtering by category
+                filtered_expenses = filter_expenses_by_date(s_date, end_date)
+
+            # Filtering by category
             elif filter_choice == "2":
                 categories_dict = {
                     "1": "Food",
@@ -149,32 +184,40 @@ def load_and_view_expenses():
                 print("\nAvailable categories for filtering:")
                 for key, value in categories_dict.items():
                     print(f"{key}. {value}")
-                category_input = input("Choose a category (1-7) or 'm' to return to the menu: ")
+                category_input = input(
+                    f"Choose a category (1-7)"
+                    f" or 'm' to return to the menu: "
+                    )
                 if category_input.lower() == 'm':
-                    break 
+                    break
                 category = categories_dict.get(category_input)
 
                 if not category:
                     print("Error: Invalid category input.")
-                    continue 
+                    continue
                 filtered_expenses = filter_expenses_by_category(category)
-            
+
             # Display all or filtered expenses
             # Show all expenses
             elif filter_choice == "3":
-                filtered_expenses = expenses  
+                filtered_expenses = expenses
 
             elif filter_choice.lower() == 'm':
-                break 
+                break
             else:
                 print("Invalid choice. Please choose '1', '2', '3' or 'm'.")
-                continue 
+                continue
 
             # Display filtered or all expenses
             if filtered_expenses:
                 print("\nSelected expenses:")
                 for expense in filtered_expenses:
-                    print(f"Amount: {expense['amount']}, Category: {expense['category']}, Date: {expense['date']}")
+                    print(
+                         f"Amount: {expense['amount']}, "
+                         f"Category: {expense['category']}, "
+                         f"Date: {expense['date']}"
+                         )
+
             else:
                 print("No expenses to display according to the filter.")
             input("\nPress Enter to return to the menu.")
@@ -183,13 +226,14 @@ def load_and_view_expenses():
         print(f"Error loading data from Google Sheets: {e}")
         return
 
+
 # Function to analyze expenses with filtering options
 def analyze_expenses():
     if not expenses:
         print("No expenses for analysis. You need to download expenses.")
         return
-    
-    filtered_expenses_for_analyze = []
+
+    filtered_expenses_analyze = []
 
     while True:
         print("\nFilter expenses:")
@@ -197,19 +241,31 @@ def analyze_expenses():
         print("2. By category")
         print("3. Analyze all records")
         print("m. Return to the menu")
-        filter_choice = input("Choose a filtering option (1-3 or 'm' to return to the menu): ")
+        filter_choice = input(
+                        f"Choose a filtering option "
+                        f"(1-3 or 'm' to return to the menu): "
+                        )
 
         # Analyze expenses by date
         if filter_choice == "1":
             try:
-                start_date = datetime.datetime.strptime(input("Enter start date (dd-mm-yyyy) or 'm' to return to the menu: "), "%d-%m-%Y").date()
-                end_date = datetime.datetime.strptime(input("Enter end date (dd-mm-yyyy) or 'm' to return to the menu: "), "%d-%m-%Y").date()
-                filtered_expenses_for_analyze = filter_expenses_by_date(start_date, end_date)
+                start_date = datetime.datetime.strptime(input(
+                    f"Enter start date (dd-mm-yyyy)"
+                    f" or 'm' to return to the menu: "), "%d-%m-%Y").date()
+                end_date = datetime.datetime.strptime(input(
+                    f"Enter end date (dd-mm-yyyy)"
+                    f" or 'm' to return to the menu: "), "%d-%m-%Y").date()
+                filtered_expenses_analyze = filter_expenses_by_date(
+                    start_date, end_date
+                )
             except ValueError:
-                print("Error: Incorrect date format. Make sure to use dd-mm-yyyy.")
+                print(
+                    f"Error: Incorrect date format."
+                    f" Make sure to use dd-mm-yyyy."
+                    )
                 continue
-        
-        # Analyze expenses by category  
+
+        # Analyze expenses by category
         elif filter_choice == "2":
             categories_dict = {
                 "1": "Food",
@@ -223,7 +279,10 @@ def analyze_expenses():
             print("\nAvailable categories for filtering:")
             for key, value in categories_dict.items():
                 print(f"{key}. {value}")
-            category_input = input("Choose a category (1-7) or 'm' to return to the menu: ")
+            category_input = input(
+                f"Choose a category (1-7)"
+                f" or 'm' to return to the menu: "
+                )
             if category_input.lower() == 'm':
                 return
             category = categories_dict.get(category_input)
@@ -231,27 +290,31 @@ def analyze_expenses():
             if not category:
                 print("Error: Invalid category input.")
                 continue
-            filtered_expenses_for_analy = filter_expenses_by_category(category)
-        
+            filtered_expenses_analyze = filter_expenses_by_category(category)
+
         # Analyze all expenses
         elif filter_choice == "3":
-            filtered_expenses_for_analy = expenses  
-        
+            filtered_expenses_analyze = expenses
+
         elif filter_choice.lower() == 'm':
             return
         else:
             print("Invalid choice. Please choose '1', '2', '3' or 'm'.")
             continue
-        
+
         # Analyzing filtered or all expenses
-        if filtered_expenses_for_analy:
-            total = sum([expense['amount'] for expense in filtered_expenses_for_analy])
+        if filtered_expenses_analyze:
+            total = sum(
+                [expense['amount'] for expense in filtered_expenses_analyze]
+                )
             print(f"\nTotal expenses: {total}")
 
             # Analyze expenses by category
             categories = {}
-            for expense in filtered_expenses_for_analy:
-                categories[expense["category"]] = categories.get(expense["category"], 0) + expense["amount"]
+            for expense in filtered_expenses_analyze:
+                categories[expense["category"]] = categories.get(
+                    expense["category"], 0
+                    ) + expense["amount"]
 
             print("\nAnalysis by category:")
             for category, amount in categories.items():
@@ -260,17 +323,22 @@ def analyze_expenses():
             print("No expenses to analyze according to the filter.")
         input("\nPress Enter to return to the menu.")
 
+
 # Filtering functions for date and category
 # Function to filter expenses by date
 def filter_expenses_by_date(start_date, end_date):
     return [
         expense for expense in expenses
-        if start_date <= datetime.datetime.strptime(expense['date'], "%d-%m-%Y").date() <= end_date
+        if start_date <= datetime.datetime.strptime(
+            expense['date'], "%d-%m-%Y"
+            ).date() <= end_date
     ]
+
 
 # Function to filter expenses by category
 def filter_expenses_by_category(category):
-    return [expense for expense in expenses if expense["category"] == category]   
+    return [expense for expense in expenses if expense["category"] == category]
+
 
 # Main menu function
 def main_menu():
@@ -294,5 +362,6 @@ def main_menu():
         else:
             print("Invalid input. Please try again.")
 
+
 # Run the application
-main_menu()  
+main_menu()
